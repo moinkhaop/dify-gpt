7bbfcc1a9571e720be6d98f13d2f592c8c51deb4
moinkhao1024 <moinkhao1024@outlook.com>
2023-12-15 21:50:38 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,14 +3,14 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
+  title: '憨包助手',
+  description: '来自科大讯飞的星火大模型 感谢云上月枫下叶的搭建',
+  copyright: '云上月枫下叶',
+  privacy_policy: '网络不是非法之地，劝你谨言慎行',
   default_language: 'zh-Hans',
 }
 
-export const isShowPrompt = false
+export const isShowPrompt = true
 export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
