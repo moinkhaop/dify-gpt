0ab9d67d26e8a896186be762e81449da7b04a1b8
moinkhao1024 <moinkhao1024@outlook.com>
2023-12-15 21:49:13 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,10 +3,10 @@ export const APP_ID = ''
 export const API_KEY = ''
 
 export const APP_INFO: AppInfo = {
-  "title": 'Chat APP',
-  "description": '',
-  "copyright": '',
-  "privacy_policy": '',
+  "title": '憨包助手',
+  "description": '来自科大讯飞的星火大模型 感谢云上月枫下叶的搭建',
+  "copyright": '云上月枫下叶',
+  "privacy_policy": '网络不是非法之地，劝你谨言慎行',
   "default_language": 'zh-Hans'
 }
 
